28a583f611b10d71bda1a88b065165ed206edaf7
Edier Gama <ediersantiago7@gmail.com>
2022-09-01 22:11:22 -0500

    Método Includes

---
+++ src/07-includes.js
@@ -0,0 +1,8 @@
+// Includes es un método que nos sirve para detectar si un array tiene
+// o no algun elemento en especifico
+
+
+const animals = ['Zorro', 'Gato', 'Perro']
+
+const rta = animals.includes('Zorro')
+console.log(rta); //true
